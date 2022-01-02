f79b0b0da309005a76a411b881c8f50147316908
Gerardo <bloodmasktv@gmail.com>
2022-01-02 13:38:16 +0100

    sql: aggiornamento database
    - aggiunto attributo poster alla tabella film;
    - resa N a N la relazione tra account e film (review)
    - modificata chiave primaria show_room (resa a due attributi, id_room e id_show)
    - refactor id_client -> id_customer
    - aggiunti ora e data alla recensione e resa chiave primaria a 2 attributi (id_customer e id_film)

--- Sql/UniCinemaDB.sql
+++ Sql/UniCinemaDB.sql
@@ -15,8 +15,8 @@ administrator boolean
 CREATE TABLE purchase (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 date_purchase date,
-id_client int,
-FOREIGN KEY(id_client) REFERENCES customer(id) ON DELETE CASCADE
+id_customer int,
+FOREIGN KEY(id_customer) REFERENCES customer(id) ON DELETE CASCADE
 );
 
 CREATE TABLE room(
@@ -32,7 +32,8 @@ duration int,
 date_publishing date,
 genre int,
 plot VARCHAR(1000),
-cover VARCHAR(100)
+cover VARCHAR(100),
+poster VARCHAR(100)
 );
 
 CREATE TABLE spectacle (
@@ -55,8 +56,9 @@ FOREIGN KEY (id_purchase) REFERENCES purchase(id) ON DELETE CASCADE
 );
 
 CREATE TABLE show_room (
-id_room INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+id_room int,
 id_show int,
+Primary Key(id_room, id_show),
 FOREIGN KEY (id_show) REFERENCES spectacle(id) ON DELETE CASCADE,
 FOREIGN KEY (id_room) REFERENCES room(id) ON DELETE CASCADE
 );
@@ -93,20 +95,24 @@ FOREIGN KEY (id_film) REFERENCES film(id) ON DELETE CASCADE
 );
 
 CREATE TABLE review (
-id_client int PRIMARY KEY NOT NULL,
+id_customer int NOT NULL,
 id_film int NOT NULL,
+title varchar(50),
 caption varchar(500),
 stars int,
-FOREIGN KEY (id_client) REFERENCES customer(id) ON DELETE CASCADE,
+rev_date date,
+rev_time time,
+Primary Key(id_customer, id_film),
+FOREIGN KEY (id_customer) REFERENCES customer(id) ON DELETE CASCADE,
 FOREIGN KEY (id_film) REFERENCES film(id) ON DELETE CASCADE
 );
 
 INSERT INTO film VALUES 
-("01","Venom - La furia di Carnage","97","2021-10-14","1","Dopo aver trovato un corpo ospite nel giornalista investigativo Eddie Brock, il simbionte alieno deve affrontare un nuovo nemico, Carnage, alter ego dell'assassino seriale Cletus Kasady.","wallpaper_venom.jpg"),
-("02","No Time To Die","163","2021-09-30","2","Dopo aver lasciato i servizi segreti, Bond si gode una vita tranquilla in Giamaica. Tuttavia, la pace conquistata si rivela di breve durata quando il suo vecchio amico Felix Leiter gli chiede aiuto.","wallpaper_NoTimeToDie.jpg"),
-("03","Avatar","120","2021-10-08","4","L'ex marine Jake Sully è stato reclutato per una missione sul pianeta Pandora con lo scopo di recuperare risorse naturali in esaurimento sulla Terra. Inaspettatamente si ritrova a voler proteggere il mondo magico al quale si sente stranamente legato.", "wallpaper_avatar.jpg"),
-("04","Inception","130","2021-10-09","5","Dom Cobb possiede una qualifica speciale: è in grado di inserirsi nei sogni altrui per prelevare i segreti nascosti nel più profondo del subconscio. Viene contattato da Saito, un potentissimo industriale giapponese.","wallpaper_inception.jpg"),
-("05","Zlatan","100","2021-11-11","5","Il film parte dai primi anni della vita di Ibra, dai difficili inizi in Svezia fino all'exploit nel calcio. Vengono narrati i momenti e le persone più significative della sua carriera.","wallpaper_zlatan.jpg");
+("01","Venom - La furia di Carnage","97","2021-10-14","1","Dopo aver trovato un corpo ospite nel giornalista investigativo Eddie Brock, il simbionte alieno deve affrontare un nuovo nemico, Carnage, alter ego dell'assassino seriale Cletus Kasady.","wallpaper_venom.jpg","poster_venom.jpg"),
+("02","No Time To Die","163","2021-09-30","2","Dopo aver lasciato i servizi segreti, Bond si gode una vita tranquilla in Giamaica. Tuttavia, la pace conquistata si rivela di breve durata quando il suo vecchio amico Felix Leiter gli chiede aiuto.","wallpaper_NoTimeToDie.jpg","poster_NoTimeToDie.jpg"),
+("03","Avatar","120","2021-10-08","4","L'ex marine Jake Sully è stato reclutato per una missione sul pianeta Pandora con lo scopo di recuperare risorse naturali in esaurimento sulla Terra. Inaspettatamente si ritrova a voler proteggere il mondo magico al quale si sente stranamente legato.", "wallpaper_avatar.jpg","poster_avatar.jpg"),
+("04","Inception","130","2021-10-09","5","Dom Cobb possiede una qualifica speciale: è in grado di inserirsi nei sogni altrui per prelevare i segreti nascosti nel più profondo del subconscio. Viene contattato da Saito, un potentissimo industriale giapponese.","wallpaper_inception.jpg","poster_inception.jpg"),
+("05","Zlatan","100","2021-11-11","5","Il film parte dai primi anni della vita di Ibra, dai difficili inizi in Svezia fino all'exploit nel calcio. Vengono narrati i momenti e le persone più significative della sua carriera.","wallpaper_zlatan.jpg","poster_zlatan.jpg");
 
 
 INSERT INTO actor VALUES
@@ -164,22 +170,25 @@ INSERT INTO director VALUES
 ("04","05","Jens","Jorgen"),
 ("05","04","Christopher","Nolan");
 
+INSERT INTO customer VALUES
+("01","gleone2000@gmail.com","78feb7296322c966b855fc42df5666a8cbb9640e3ea626e082f72d77fc6e0f2001271e5134fc4f95808bc96773f0782991a804cddc1a150587df56026d40e268","Gerardo","Leone",false,true),
+("02","mlezzi@gvkd.com",null,"Mario","Lezzi",false,false),
+("03","suosso@gvkd.com",null,"Antonio","Santosuosso",false,false);
+
 
 INSERT INTO spectacle VALUES
-("01","16:00:00","2021-10-17","01"),
-("02","18:00:00","2021-10-17","01"),
-("03","20:00:00","2021-10-17","01"),
-("04","18:00:00","2021-10-01","02"),
-("05","20:30:00","2021-10-01","02"),
-("06","17:00:00","2021-10-10","03"),
-("08","19:30:00","2021-10-10","03"),
-("09","19:30:00","2021-11-11","05"),
-("10","21:30:00","2021-11-11","05");
-
-INSERT INTO customer VALUES 
-("01","a@gmail.com",NULL,"Antonio","Santosuosso","0","1"),
-("02","g@gmail.com",NULL,"Gerardo","Leone","0","1"),
-("03","m@gmail.com",NULL,"Mario","Lezzi","0","1");
+("01","16:00:00","2022-10-17","01"),
+("02","18:00:00","2022-10-17","01"),
+("03","20:00:00","2022-10-17","01"),
+("04","18:00:00","2022-10-01","02"),
+("05","20:30:00","2022-10-01","02"),
+("06","17:00:00","2022-10-10","03"),
+("08","19:30:00","2022-10-10","03"),
+("09","19:30:00","2022-11-11","05"),
+("10","21:30:00","2022-11-11","05"),
+("11","22:30:00","2022-11-11","01"),
+("12","23:30:00","2022-11-11","01"),
+("13","20:30:00","2022-11-11","01");
 
 INSERT INTO purchase VALUES 
 ("01","2021-10-17","01"),
@@ -200,7 +209,10 @@ INSERT INTO room VALUES
 INSERT INTO show_room VALUES
 ("01","01"),
 ("02","08"),
-("03","05");
+("03","05"),
+("01", "11"),
+("01", "12"),
+("01", "13");
 
 INSERT INTO review VALUES 
-("01","02","Film Eccellente","5");
+("01","02","Film Eccellente","Un film davvero con i fiocchi, ben prodotto e registrato","5","2021-12-27","01:36:00");
