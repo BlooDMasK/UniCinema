add9e40d56779f2b6a9d294bb85e14794d0b23dd
Gerardo <bloodmasktv@gmail.com>
2022-02-11 21:54:50 +0100

    fix: pattern validazione form film

--- Project IntelliJ/src/main/java/utils/validator/FilmValidator.java
+++ Project IntelliJ/src/main/java/utils/validator/FilmValidator.java
@@ -20,24 +20,24 @@ public class FilmValidator {
         requestValidator.assertMatchArray("DirectorsLastname", Pattern.compile("^[A-Za-z-,'àèìòù ]{2,30}$"), "Il cognome non è valido.");
         requestValidator.assertMatchArray("ProductionFirstname", Pattern.compile("^[A-Za-z-,'àèìòù ]{2,30}$"), "Il nome non è valido.");
         requestValidator.assertMatchArray("ProductionLastname", Pattern.compile("^[A-Za-z-,'àèìòù ]{2,30}$"), "Il cognome non è valido.");
-        requestValidator.assertMatchArray("HouseProductionName", Pattern.compile("^[A-Za-z0-9\\W]{5,50}$"), "Il nome non è valido.");
+        requestValidator.assertMatchArray("HouseProductionName", Pattern.compile("^[0-9A-Za-z-àèìòù ]{2,30}$"), "Il nome non è valido.");
 
         return requestValidator;
     }
 
     public static RequestValidator validateUpdateFilm(HttpServletRequest request) throws ServletException, IOException {
         RequestValidator requestValidator = new RequestValidator(request);
-        requestValidator.assertMatch("title", Pattern.compile("^[A-Za-z0-9\\W]{6,50}$"), "Il titolo non è valido.", true);
+        requestValidator.assertMatch("title", Pattern.compile("^.[^{}\\[\\];_]{6,50}$"), "Il titolo non è valido.", true);
         requestValidator.assertIntRange("length", 10, 999, "La durata deve essere di minimo 10, massimo 999 minuti.", true);
         requestValidator.assertIntRange("genre", 1, 15, "Il genere deve essere compreso tra 1 e 15.", true);
-        requestValidator.assertMatch("plot", Pattern.compile("(?!(\\[|\\]|\\{|\\}|\\@|\\_|\\=))[A-Za-z0-9\\W]{10,1000}"), "La trama non è valida.", true);
+        requestValidator.assertMatch("plot", Pattern.compile("^.[^\\[\\]{}@=_?]{10,1000}$"), "La trama non è valida.", true);
         requestValidator.assertMatchArray("ActorsFirstname", Pattern.compile("^[A-Za-z-,'àèìòù ]{2,30}$"), "Il nome non è valido.");
         requestValidator.assertMatchArray("ActorsLastname", Pattern.compile("^[A-Za-z-,'àèìòù ]{2,30}$"), "Il cognome non è valido.");
         requestValidator.assertMatchArray("DirectorsFirstname", Pattern.compile("^[A-Za-z-,'àèìòù ]{2,30}$"), "Il nome non è valido.");
         requestValidator.assertMatchArray("DirectorsLastname", Pattern.compile("^[A-Za-z-,'àèìòù ]{2,30}$"), "Il cognome non è valido.");
         requestValidator.assertMatchArray("ProductionFirstname", Pattern.compile("^[A-Za-z-,'àèìòù ]{2,30}$"), "Il nome non è valido.");
         requestValidator.assertMatchArray("ProductionLastname", Pattern.compile("^[A-Za-z-,'àèìòù ]{2,30}$"), "Il cognome non è valido.");
-        requestValidator.assertMatchArray("HouseProductionName", Pattern.compile("^[A-Za-z0-9\\W]{5,50}$"), "Il nome non è valido.");
+        requestValidator.assertMatchArray("HouseProductionName", Pattern.compile("^[0-9A-Za-z-àèìòù ]{2,30}$"), "Il nome non è valido.");
 
         return requestValidator;
     }
